5fc9c28f98478ba124ff7b8381a86d92f7a86ec6
hasan <hasan.naseem2@gmail.com>
2021-03-09 10:32:27 +0500

    Initial Backend

---
+++ Rigged_backend/src/product/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+
+class Product(models.Model):
+
+    CONDITION_TYPE = (
+        ("New", "New"),
+        ("Used", "Used")
+    )
+    ## will contain product information
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User , on_delete = models.CASCADE)
+    description = models.TextField(max_length=500)
+    condition = models.CharField(max_length=100, choices = CONDITION_TYPE)
+    category = models.ForeignKey('Category', on_delete = models.SET_NULL, null = True)
+    brand = models.ForeignKey('Brand', on_delete = models.SET_NULL, null = True)
+    price = models.DecimalField(max_digits=10, decimal_places = 5)
+    created = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.name
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', blank = True, null = True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
+
+    
+
+class Category(models.Model):
+    ## for product category
+
+    category_name = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = 'category/', blank = True, null = True) # blank and null = True enable the user to leave this field
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+
+
+class Brand(models.Model):
+    ## for product category
+
+    brand_name = models.CharField(max_length = 50)
+    
+    class Meta:
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
+
+    def __str__(self):
+        return self.brand_name        
