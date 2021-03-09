9f1ad21ec04f24c7b6c57a49640a9ffeaa892b1b
hasan <hasan.naseem2@gmail.com>
2021-03-09 23:54:02 +0500

    static media files implemented in backend

--- Rigged_backend/src/olx/settings.py
+++ Rigged_backend/src/olx/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
-
+from pathlib import Path 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -76,16 +75,16 @@ WSGI_APPLICATION = 'olx.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE' : 'sql_server.pyodbc',
-        'PORT' : '1433',
-        'NAME' : 'Test',
-        'PASSWORD' : 'rigged',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-            'unicode_results': True,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+        # 'ENGINE' : 'sql_server.pyodbc',
+        # 'PORT' : '1433',
+        # 'NAME' : 'Test',
+        # 'PASSWORD' : 'rigged',
+        # 'OPTIONS': {
+        #     'driver': 'ODBC Driver 17 for SQL Server',
+        #     'unicode_results': True,
+        # },
     }
 }
 
@@ -127,3 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,  "static"),
+   
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
