9daaeadca83add069446ef2bee4ea8ee51697361
hsntrq <mt05491@st.habib.edu.pk>
2021-03-16 13:49:45 +0500

    procfile

--- olx/settings.py
+++ olx/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qh66)5xtx3somb(_rnadl7t(=_6q*mi$ow5a(l%a!ev(k1o-ry'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rigged.herokuapp.com']
 
 
 # Application definition
