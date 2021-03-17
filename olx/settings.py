57f466c1f9014eca1ae58da2d7fa94b8f04508a1
Graceful Skipper <49026436+kk05088@users.noreply.github.com>
2021-03-17 23:56:42 +0500

    Added DNS custom hosts and allowed access

--- olx/settings.py
+++ olx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
-ALLOWED_HOSTS = ['rigged.herokuapp.com']
+ALLOWED_HOSTS = ['rigged.herokuapp.com', 'rigged.rocks', 'www.rigged.rocks']
 
 
 # Application definition
