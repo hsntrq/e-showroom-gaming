badb06613111d3ebc7960a26cd241fdd59c2d570
hasan <hasan.naseem2@gmail.com>
2021-04-01 12:26:49 +0500

    Sprint 2 final code

--- olx/urls.py
+++ olx/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('pages.urls')),
     path('home/', include('product.urls', namespace='products')),    
     
     
