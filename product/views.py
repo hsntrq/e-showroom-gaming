22f88d7f663dc8eaa7735548f8dd5cb1a1a07f6b
hasan <hasan.naseem2@gmail.com>
2021-04-01 09:00:46 +0500

    search, sort implemented

--- product/views.py
+++ product/views.py
@@ -15,37 +15,75 @@ from django.db.models import Count
 
 # ******************************************************************************************************
 def productlist(request):
+    CategoryList = models.Category.objects.all()
     productlist = models.Product.objects.all().order_by('featured') # will retrieve all the products in our database
     
+    # search_query = request.GET.get('q')
+    # if search_query:
+    #     print(search_query) # will print search query in cd
+    #     productlist = productlist.filter(
+    #         Q(name__icontains = search_query) | 
+    #         Q(description__icontains = search_query) | 
+    #         Q(condition__icontains = search_query) 
+    #         # Q(brand__brand_name__icontains = search_query)
+    #     )
+
+    
+    template = 'Product/product_list.html'
+    context = {'category_list' : CategoryList, 'product_list':productlist}
+    
+    return render(request, template, context)
+
+def search(request):
+
+    productlist = models.Product.objects.all() # will retrieve all the products in our database
+    CategoryList = models.Category.objects.annotate(total_products = Count('product'))
+
     search_query = request.GET.get('q')
     if search_query:
         print(search_query) # will print search query in cd
         productlist = productlist.filter(
             Q(name__icontains = search_query) | 
             Q(description__icontains = search_query) | 
-            Q(condition__icontains = search_query) 
+            Q(condition__icontains = search_query) |
+            Q(brand__icontains = search_query) 
             # Q(brand__brand_name__icontains = search_query)
         )
 
-    
-    template = 'Product/product_list.html'
-    context = {'product_list' : productlist}
-    
-    return render(request, template, context)
+    category_query = request.GET.get('category')
+    if category_query:
+        category = models.Category.objects.all()
+        print(category_query)
+        for c in category:
+            if category_query == c.category_name:
+                category_id = c.id
+                print(category_id)
+                break
+        productlist = productlist.filter(category = category_id)
 
-def categorylist(request):
+    price_query = request.GET.get('price')
+    if price_query:
+        print(price_query)
+        price_low, price_high = [int(x) for x in price_query.split()]
+        productlist = productlist.filter(price__range = (price_low, price_high))
+        
+    sort_query = request.GET.get('sort')
+    if sort_query:
+        print(sort_query)
+        if sort_query == "name":
+            productlist = productlist.order_by('name')
+        elif sort_query == "date":
+            productlist = productlist.order_by('-created')
+        elif sort_query == "priceh":
+            productlist = productlist.order_by('-price')
+        elif sort_query == "pricel":
+            productlist = productlist.order_by('price')
 
-    CategoryList = models.Category.objects.annotate(total_products = Count('product'))
-    productlist = models.Product.objects.all().order_by('featured') # will retrieve all the products in our database
-
-
-    context = {'category_list' : CategoryList}
-    context = {'product_list' : productlist}
-def search(request):
-    productlist = models.Product.objects.all()
-    context = {'product_list' : productlist}
     template = 'Product/search.html'
+    context = {'category_list' : CategoryList, 'product_list':productlist}
     return render(request, template, context)
+    
+
 
 
 def chat(request):
@@ -53,13 +91,14 @@ def chat(request):
     return render(request, template)
 
 def productdetail(request, product_slug):
+    CategoryList = models.Category.objects.all()
     # print(product_slug)
     
     # print(product_slug)
     productdetail = models.Product.objects.get(slug = product_slug)
     productimages = models.ProductImages.objects.filter(product=productdetail)
     template = 'Product/product_detail.html'
-    context = {'product_detail' : productdetail, 'product_images' : productimages}
+    context = {'product_detail' : productdetail, 'product_images' : productimages, 'category_list' : CategoryList}
     return render(request, template, context)
 
 
@@ -68,15 +107,19 @@ def productdetail(request, product_slug):
 
 
 def create(request):
-        form = PostAd(request.POST, request.FILES)
-        print(form)
-        if form.is_valid():
-            print('yes')
-            form.save()
-            form = PostAd(request.POST, request.FILES)
-        else:
-            print('no')
-        context = {
-            'form':form
-        }
-        return render(request, 'Product/post.html', context)
+
+    CategoryList = models.Category.objects.all()
+    form = PostAd(request.POST, request.FILES)
+    print(form)
+    if form.is_valid():
+        print('yes')
+        form.save()
+        # form = PostAd(request.POST, request.FILES)
+        return redirect('/home')
+    else:
+        print('no')
+    context = {
+        'form':form, 
+        'category_list' : CategoryList,
+    }
+    return render(request, 'Product/post.html', context)
