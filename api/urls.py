a3895d55f44aa49b0470000e62c5a82439a05cf4
hsntrq <mt05491@st.habib.edu.pk>
2021-04-12 02:22:06 +0500

    created api.py

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
-from . import views
+from . import api
 from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'product'
 
 urlpatterns = [
     
-    path('products/', views.ProductListView.as_view(), name = 'products'),
-    path('create/', views.ProductCreateView.as_view(), name = 'post'),
-    # path('search/', views.SearchAPIView.as_view(), name = 'search'),
-    path('product/', views.ProductView.as_view(), name = 'product'),
+    path('products/', api.ProductListView.as_view(), name = 'products'),
+    path('create/', api.ProductCreateView.as_view(), name = 'post'),
+    # path('search/', api.SearchAPIView.as_view(), name = 'search'),
+    path('product/', api.ProductView.as_view(), name = 'product'),
     
-    path('', views.productlist, name = 'product_list'),
-    path('ad/<slug:product_slug>', views.productdetail, name = 'product_detail'),
-    path('post/', views.create, name="post"),
-    path('search/', views.search, name="search"),
-    path('chat/', views.chat, name="chat"),
+    path('', api.productlist, name = 'product_list'),
+    path('ad/<slug:product_slug>', api.productdetail, name = 'product_detail'),
+    path('post/', api.create, name="post"),
+    path('search/', api.search, name="search"),
+    path('chat/', api.chat, name="chat"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
