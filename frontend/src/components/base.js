f85e86fe84e096953306bf5eba4d864d6950c701
hsntrq <mt05491@st.habib.edu.pk>
2021-04-13 18:22:20 +0500

    base isolated

--- frontend/src/components/base.js
+++ frontend/src/components/base.js
@@ -7,30 +7,14 @@ export class Navbar extends Component {
       <nav className="navbar navbar-light navbar-expand sticky-top navigation-clean">
         <div className="container">
           <a className="navbar-brand" href="/">
-            <img
-              src="/static/assets/img/logo.png"
-              alt="RIGGED logo"
-              style={{height: "50px"}}
-            />
+            <img src="/static/assets/img/logo.png" alt="RIGGED logo" style={{height: "50px"}}/>
           </a>
           <div className="dropdown" style={{marginRight: "auto"}}>
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               All Categories
             </button>
             <form id="category_menu" method="GET" action="{% url 'api:search' %}">
-              <ul
-                className="dropdown-menu"
-                name="category"
-                style={{marginBottom: "5px"}}
-                aria-labelledby="dropdownMenuButton"
-              >
+              <ul className="dropdown-menu" name="category" style={{marginBottom: "5px"}} aria-labelledby="dropdownMenuButton">
                 {/* <li>
                   <a
                     className="dropdown-item"
@@ -43,29 +27,14 @@ export class Navbar extends Component {
               </ul>
             </form>
           </div>
-          <form
-            id="searchbar"
-            className="input-group"
-            method="GET"
-            action="/search"
-            style={{width: "30%"}}
-          >
-            <input
-              type="text"
-              aria-label="First name"
-              className="form-control"
-              placeholder="What are you looking for..."
-              name="q"
-              required
-            />
+          <form id="searchbar" className="input-group" method="GET" action="/search" style={{width: "30%"}}>
+            <input type="text" aria-label="First name" className="form-control" placeholder="What are you looking for..." name="q" required/>
             <div className="input-group-prepend" style={{cursor: "pointer"}}>
               <button type="submit" className="input-group-text">Search</button>
             </div>
           </form>
   
-          <a className="btn btn-primary ml-auto nav-b" role="button" id="login-b">
-            Log In | Sign Up
-          </a>
+          <a className="btn btn-primary ml-auto nav-b" role="button" id="login-b">Log In | Sign Up</a>
           <a className="btn btn-primary nav-b" role="button" id="post-ad">Post Ad</a>
         </div>
       </nav>
