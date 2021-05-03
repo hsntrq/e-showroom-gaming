e6053a2c072cdc6a6033b89b12c40824e109506a
hsntrq <mt05491@st.habib.edu.pk>
2021-05-03 11:33:00 +0500

    cart quantity

--- frontend/src/components/cart.js
+++ frontend/src/components/cart.js
@@ -7,151 +7,163 @@ export default class Cart extends Component {
     this.state = {
       products: [],
     };
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch("/api/cart", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          products: data,
+        });
+      });
+  }
+
+  updatequantity(index, inc, slug, product_quantity) {
+    let box = document.getElementById("textbox" + index);
+    if (inc && product_quantity == box.value+1){
+      alert("Stock finished");
+    }
+    else if (inc == false && box.value == 1){
+      alert("quantity can not be empty")
+    }
+    else{
+    let increase = inc ? "True" : "False";
+    console.log(slug);
+    fetch("/api/updatequantity/", {
+      method: "POST",
+      body: JSON.stringify({ increase: increase, slug: slug }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => {
+        return response.status == 201;
+      })
+      .then((status) => {
+        if (status) {
+          let ch = inc ? 1:-1;
+          box.value = parseInt(box.value) + ch;
+        }
+      });
+    }
+  }
+
+  totalPrice(){
+    let price = 0;
+    this.state.products.map(index => {
+      price += item.price;
+    });
+    console.log(price);
+    return price
   }
 
   render() {
     return (
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-md-10 col-11 mx-auto">
-            <div class="row mt-5 gx-3">
-              <div class="col-md-12 col-lg-8 col-11 mx-auto main_cart mb-lg-0 mb-5 shadow">
-                <div class="card p-4">
-                  <div class="row">
-                    <div class="col-md-5 col-11 mx-auto bg-light d-flex justify-content-center align-items-center shadow product_img">
-                      <img
-                        src="images/img2.png"
-                        class="img-fluid"
-                        alt="cart img"
-                      />
-                    </div>
-                    <div class="col-md-7 col-11 mx-auto px-4 mt-2">
-                      <div class="row">
-                        <div class="col-6 card-title">
-                          <h1 class="mb-4 product_name">Product Name</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-10 col-11 mx-auto">
+            <div className="row mt-5 gx-3">
+              <div className="col-md-12 col-lg-8 col-11 mx-auto main_cart mb-lg-0 mb-5 shadow">
+                {this.state.products.map((item, index) => (
+                  <div key={index}>
+                    <div className="card p-4">
+                      <div className="row">
+                        <div className="col-md-5 col-11 mx-auto bg-light d-flex justify-content-center align-items-center shadow product_img">
+                          <img
+                            src={item.image}
+                            className="img-fluid"
+                            alt="cart img"
+                          />
                         </div>
-                        <div class="col-6">
-                          <ul class="pagination justify-content-end set_quantity">
-                            <li class="page-item">
-                              <button
-                                class="page-link "
-                                onclick="decreaseNumber('textbox','itemval')"
-                              >
-                                <i class="fas fa-minus"></i>{" "}
-                              </button>
-                            </li>
-                            <li class="page-item">
-                              <input
-                                type="text"
-                                name=""
-                                class="page-link"
-                                value="1"
-                                id="textbox"
-                              />
-                            </li>
-                            <li class="page-item">
-                              <button
-                                class="page-link"
-                                onclick="increaseNumber('textbox','itemval')"
-                              >
-                                {" "}
-                                <i class="fas fa-plus"></i>
-                              </button>
-                            </li>
-                          </ul>
+                        <div className="col-md-7 col-11 mx-auto px-4 mt-2">
+                          <div className="row">
+                            <div className="col-6 card-title">
+                              <h1 className="mb-4 product_name">
+                                {item.product}
+                              </h1>
+                            </div>
+                            <div className="col-6">
+                              <ul className="pagination justify-content-end set_quantity">
+                                <li className="page-item">
+                                  <button
+                                    className="page-link "
+                                    id = {"minus"+index}
+                                    onClick={() => this.updatequantity(
+                                      index,
+                                      false,
+                                      item.slug,
+                                      item.product_quantity,
+                                    )}
+                                  >
+                                    <i className="fas fa-minus"></i>{" "}
+                                  </button>
+                                </li>
+                                <li className="page-item">
+                                  <input
+                                    type="text"
+                                    name=""
+                                    className="page-link"
+                                    value={item.quantity}
+                                    id={"textbox" + index}
+                                  />
+                                </li>
+                                <li className="page-item">
+                                  <button
+                                    className="page-link"
+                                    id = {"plus"+index}
+                                    onClick={() => this.updatequantity(
+                                      index,
+                                      true,
+                                      item.slug,
+                                      item.product_quantity,
+                                    )}
+                                  >
+                                    {" "}
+                                    <i className="fas fa-plus"></i>
+                                  </button>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-4 d-flex justify-content-end price_money">
+                              <h3>
+                                Rs. <span id="itemval">{item.price}</span>
+                              </h3>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                      <div class="row">
-                        <div class="col-4 d-flex justify-content-end price_money">
-                          <h3>
-                            Rs. <span id="itemval">0.00 </span>
-                          </h3>
-                        </div>
-                      </div>
                     </div>
+                    <hr />
                   </div>
-                </div>
-                <hr />
-                <div class="card p-4">
-                  <div class="row">
-                    <div class="col-md-5 col-11 mx-auto bg-light d-flex justify-content-center align-items-center shadow product_img">
-                      <img
-                        src="images/img3.png"
-                        class="img-fluid"
-                        alt="cart img"
-                      />
-                    </div>
-                    <div class="col-md-7 col-11 mx-auto px-4 mt-2">
-                      <div class="row">
-                        <div class="col-6 card-title">
-                          <h1 class="mb-4 product_name">Product Name</h1>
-                        </div>
-                        <div class="col-6">
-                          <ul class="pagination justify-content-end set_quantity">
-                            <li class="page-item">
-                              <button
-                                class="page-link "
-                                onclick="decreaseNumber('textbox1','itemval1')"
-                              >
-                                {" "}
-                                <i class="fas fa-minus"></i>{" "}
-                              </button>
-                            </li>
-                            <li class="page-item">
-                              <input
-                                type="text"
-                                name=""
-                                class="page-link"
-                                value="1"
-                                id="textbox1"
-                              />
-                            </li>
-                            <li class="page-item">
-                              <button
-                                class="page-link"
-                                onclick="increaseNumber('textbox1','itemval1')"
-                              >
-                                {" "}
-                                <i class="fas fa-plus"></i>
-                              </button>
-                            </li>
-                          </ul>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-4 d-flex justify-content-end price_money">
-                          <h3>
-                            Rs. <span id="itemval1">0.00 </span>{" "}
-                          </h3>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                ))}
               </div>
-              <div class="col-md-12 col-lg-4 col-11 mx-auto mt-lg-0 mt-md-5">
-                <div class="right_side p-3 shadow bg-white">
-                  <h2 class="product_name mb-5">The Total Amount Of</h2>
-                  <div class="price_indiv d-flex justify-content-between">
+              <div className="col-md-12 col-lg-4 col-11 mx-auto mt-lg-0 mt-md-5">
+                <div className="right_side p-3 shadow bg-white">
+                  <h2 className="product_name mb-5">The Total Amount Of</h2>
+                  <div className="price_indiv d-flex justify-content-between">
                     <p>Product amount</p>
                     <p>
-                      Rs. <span id="product_total_amt">0.00</span>
+                      Rs. <span id="product_total_amt">{()=> this.totalPrice()}</span>
                     </p>
                   </div>
-                  <div class="price_indiv d-flex justify-content-between">
+                  <div className="price_indiv d-flex justify-content-between">
                     <p>Shipping Charge</p>
                     <p>
                       Rs. <span id="shipping_charge">50.0</span>
                     </p>
                   </div>
                   <hr />
-                  <div class="total-amt d-flex justify-content-between font-weight-bold">
+                  <div className="total-amt d-flex justify-content-between font-weight-bold">
                     <p>The total amount of (including VAT)</p>
                     <p>
                       Rs. <span id="total_cart_amt">0.00</span>
                     </p>
                   </div>
-                  <button class="btn btn-primary text-uppercase">
+                  <button className="btn btn-primary text-uppercase">
                     {" "}
                     Proceed to checkout
                   </button>
