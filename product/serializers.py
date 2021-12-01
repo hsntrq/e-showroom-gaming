69fa1a635d4c6bfbf5b632f4da699e286c7daa3f
hasan <hasan.naseem2@gmail.com>
2021-12-02 02:38:59 +0500

    serializers

---
+++ product/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from . import models
+from . import forms
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # category = serializers.PrimaryKeyRelatedField(queryset=models.Category.objects.all())
+    # category = serializers.StringRelatedField(many=True)
+    # category = CategorySerializer(many=True, read_only=True)
+    class Meta:
+        model = models.Product
+        fields = [
+            'name',
+            'owner',
+            'description',
+            'condition',
+            'category',
+            'brand',
+            'price',
+            'image',
+            'created',
+            'featured',
+            'slug'
+        ]
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = [
+            'category_name',
+        ]
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = [
+            'name',
+            'description',
+            'condition',
+            'category',
+            'brand',
+            'price',
+            'image',
+            'featured'
+        ]
