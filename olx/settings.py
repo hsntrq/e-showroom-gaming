4fc0f6a889b5ae2ab0b776751bc60c108d6145e7
hasan <hasan.naseem2@gmail.com>
2021-12-01 23:45:17 +0500

    Updated Secret Key

--- olx/settings.py
+++ olx/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "R8vFZhXmT8"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
